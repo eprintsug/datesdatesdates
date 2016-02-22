4c72945cd771b3a514588554f49151cc771b5178
jesusbagpuss <j.salter@leeds.ac.uk>
2016-02-22 15:05:27 +0000

    Map published date as best, published_online as fallback
    http://www.rioxx.net/schema/v2.0/rioxx/rioxxterms_.html#publication_date
    refers to the publication_date as:
    the publication date of the resource in the form in which it would be
    cited. This element is not used in a RIOXX context but allows for a
    RIOXX record to become a reasonable bibliographic record for the
    resource. This is a free-text field.

--- lib/cfg.d/zzz_datesdatesdates_rioxx2.pl
+++ lib/cfg.d/zzz_datesdatesdates_rioxx2.pl
@@ -21,11 +21,21 @@ $c->{rioxx2_value_dateAccepted} = sub {
 $c->{rioxx2_value_publication_date} = sub {
 	my( $eprint ) = @_;
 
+	# attempt to get the 'best' date - see description:
+	# http://www.rioxx.net/schema/v2.0/rioxx/rioxxterms_.html#publication_date
+	my $pub_online_date;
+	
 	for( @{ $eprint->value( "dates" ) } )
 	{
 		next unless defined $_->{date_type};
-		next unless $_->{date_type} eq "published_online";
+		$pub_online_date = $_->{date} if ( $_->{date_type} eq "published_online" );
+		
+		# return published date as best match
+		next unless $_->{date_type} eq "published";
 		return $_->{date};
 	}
+	#return published_online date as second best
+	return $pub_online_date if ( defined $pub_online_date );
+	
 	return undef;
 };
